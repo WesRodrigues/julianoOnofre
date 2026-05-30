8b1fcfb918e362d4f7171e5eaea0414805e6ff02
Wesley Rodrigues <wesleyoliveirasrwro@gmail.com>
2026-05-30 10:48:29 -0300

    Primeira versão da landing page

---
+++ js/cabecalho.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerBtn = document.getElementById('hamburger-btn');
+    const navMenu = document.getElementById('nav-menu');
+
+    hamburgerBtn.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+    });
+});
